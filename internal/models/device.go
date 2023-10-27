d11b2a85808ebc0908e570bf84cc7e1219f2a1df
Panitnun-6243 <panitnun.6243@gmail.com>
2023-10-27 21:17:08 +0700

    feat: add user and device code model

--- internal/models/device.go
+++ internal/models/device.go
@@ -1,1 +1,7 @@
 package models
+
+type DeviceRegistrationCode struct {
+	ID     string `bson:"_id" json:"id"`
+	Code   string `bson:"code" json:"code"`
+	UsedBy string `bson:"used_by" json:"used_by,omitempty"` // This will be a user ID.
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type User struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Email      string             `bson:"email" json:"email" validate:"required,email"`
+	Password   string             `bson:"password" json:"-" validate:"required"`
+	Name       string             `bson:"name" json:"name" validate:"required"`
+	DeviceCode string             `bson:"device_code" json:"device_code" validate:"required"`
+	AvatarURL  string             `bson:"avatar_url" json:"avatar_url" validate:"required,url"`
+	CreatedAt  time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt  time.Time          `bson:"updated_at" json:"updated_at"`
+}
