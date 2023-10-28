fe002f7e6f71cea6885b4be5618f82e17383187a
Panitnun-6243 <panitnun.6243@gmail.com>
2023-10-29 06:37:19 +0700

    feat: add light control model

--- internal/models/light_control.go
+++ internal/models/light_control.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type LightControl struct {
+	ID                   primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserID               primitive.ObjectID `bson:"user_id" json:"user_id"`
+	BrightnessPercentage float64            `bson:"brightness_percentage" json:"brightness_percentage"`
+	ColorMode            string             `bson:"color_mode" json:"color_mode"`
+	CctTemp              int                `bson:"cct_temp" json:"cct_temp"`
+	RgbColor             string             `bson:"rgb_color" json:"rgb_color"`
+	CreatedAt            time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt            time.Time          `bson:"updated_at" json:"updated_at"`
+}
